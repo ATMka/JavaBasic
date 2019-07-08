60b6ccf610399323cdaccf9c151bb63f0694699e
atmka <corp.dvd@gmail.com>
2019-07-08 19:33:15 +0400

    Задача 1

---
+++ src/String1/Main.java
@@ -0,0 +1,8 @@
+package String1;
+
+public class Main {
+    public static void main(String[] args) {
+        helloName name = new helloName();
+        System.out.println(name.getHelloName("Bob"));
+    }
+}
