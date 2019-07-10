3fd5fc8f6bc1c37004667c23dfdc2f77ce0a9213
atmka <226408>
2019-07-10 17:11:28 +0400

    string 2 first class

---
+++ src/String2/doubleChar.java
@@ -0,0 +1,11 @@
+package String2;
+
+public class doubleChar {
+    public static String doubleChar(String str) {
+        String result="";
+        for(int i = 0;i <= str.length()-1;i++){
+            result = result + str.charAt(i) + str.charAt(i);
+        }
+        return result;
+    }
+}
